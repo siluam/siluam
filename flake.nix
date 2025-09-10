556e38b4a1bde8ca326ba11febc4b274ca8270d5
Jeet Ray <jeet.ray@syvl.org>
2025-09-10 01:00:04 -0400

    Switched back to `nixos-25.05`

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs.flake-compat.url = "github:edolstra/flake-compat";
   inputs.flake-compat.flake = false;
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
   outputs = { ... }:
     let
       siluam = {
